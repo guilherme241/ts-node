83dc5116c2ba2c9be9dba63279fc44becc8e31b5
guilherme241 <guilhermerodriguesm4@gmail.com>
2022-09-02 17:05:26 -0300

    fistcomit

---
+++ dist/CreateCourseService.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class CreateCourseService {
+    execute({ name, duration, educator }) {
+        console.log(name, duration, educator);
+    }
+    ;
+}
+;
+exports.default = new CreateCourseService;
